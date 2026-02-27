67bfca5f00ca89446c267bd3cb41e20f582ee8a8
Andrea Botello <andrea.botellor@misena.edu.co>
2026-02-27 12:06:37 -0500

    Ajustes CFA [1.2]

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -26,7 +26,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://youtu.be/5hgZb-taKwQ?si=LWn67eh2lgJKP0ib" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/5hgZb-taKwQ?si=P06oGWKq6FecC_Ob" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video. Colaboración digital con Microsoft 365
 
 
