881609151756148f4ca31d145f498d0080aebeef
Andrea Botello <abotellod@gmail.com>
2026-05-26 10:52:08 -0500

    Hall. Repo [1.0]

--- src/config/global.js
+++ src/config/global.js
@@ -289,8 +289,9 @@ export default {
       autores: [
         {
           nombre: 'Claudia Johanna Gómez Pérez',
-          cargo: 'Responsable del ecosistema',
-          centro: 'Dirección General',
+          cargo:
+            'Responsable Ecosistema de Recursos Educativos Digitales (RED)',
+          centro: 'Centro Agroturístico - Regional Santander',
         },
         {
           nombre: 'Edison Eduardo Mantilla Cuadros',
